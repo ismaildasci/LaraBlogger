40743141c171aadc60b1608cbe84cc6ae0b3d541
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-26 13:44:29 +0300

    eager-load

--- routes/web.php
+++ routes/web.php
@@ -18,27 +18,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category','author')->get()
+        'posts' => Post::latest()
+            // ->with(['category', 'author'])
+            ->get(),
     ]);
 });
 
 Route::get('posts/{post:slug}', function (Post $post) {
-
-
     return view('post', [
-        'post' => $post
+        'post' => $post,
     ]);
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        // 'posts' => $category->posts->load(['category', 'author']),
+        'posts' => $category->posts,
     ]);
 });
 
-
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        // 'posts' => $author->posts->load(['category', 'author']),
+        'posts' => $author->posts,
     ]);
 });
