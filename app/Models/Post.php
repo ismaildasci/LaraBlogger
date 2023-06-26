c07c699d9559336404b6a22d4d4209fcd5a12070
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-26 10:44:01 +0300

    post-metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,28 +2,45 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post extends Model
 {
     use HasFactory;
 
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function test()
     {
-       $files = File::files(resource_path("posts/"));
-       return array_map(fn ($file) => $file->getContents(), $files);
+        return collect(File::files(resource_path("posts")))
+             ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+             ->map(fn ($document) => new Post(
+                 $document->title,
+                 $document->excerpt,
+                 $document->date,
+                 $document->body(),
+                 $document->slug
+             ));
     }
 
     public static function find($slug)
     {
-        if(!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+        return static::test()->firstWhere('slug', $slug);
     }
 }
