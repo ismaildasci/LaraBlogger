ed368fc093b7593269c8957d53f77151766d6f13
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-26 09:03:39 +0300

    route-and-link

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe molestias eius, vitae, perferendis placeat aperiam pariatur id ratione excepturi ipsa earum, quidem vel magnam tenetur illum cupiditate! Quo, sunt? Quia?</p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe molestias eius, vitae, perferendis placeat aperiam pariatur id ratione excepturi ipsa earum, quidem vel magnam tenetur illum cupiditate! Quo, sunt? Quia?</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts">My Second Post</a></h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur excepturi nihil voluptatum possimus nostrum ex? Dolor accusamus repudiandae quae ullam recusandae, voluptas eveniet illo perferendis dolorem quos excepturi alias provident.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts">My Third Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe molestias eius, vitae, perferendis placeat aperiam pariatur id ratione excepturi ipsa earum, quidem vel magnam tenetur illum cupiditate! Quo, sunt? Quia?</p>
+    </article>
+</body>
