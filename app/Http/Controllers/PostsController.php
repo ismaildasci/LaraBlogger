3a0afb61ce10f381d65f276c057b1f47e02a7243
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-26 20:10:33 +0300

    simple-pagination

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,7 +9,7 @@ class PostsController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search','category','author']))->get(),
+            'posts' => Post::latest()->filter(request(['search','category','author']))->paginate(6)->withQueryString(),
         ]);
 
     }
