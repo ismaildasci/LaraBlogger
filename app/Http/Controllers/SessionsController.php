eeac67a76cd585041ef555c782064839630a4f28
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-26 22:08:06 +0300

    build-logIn-page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:7|max:255',
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // throw ValidationException::withMessages([
+        //     'email' => 'Your provided credentials could not be verified.'
+        // ]);
+
+        return back()->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
+
     public function destroy()
     {
         auth()->logout();
