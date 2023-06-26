d3e8108ab4a7ad8010f693bd7fb7176cbe7f1d81
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-26 10:58:20 +0300

    blade-basics

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,14 +4,18 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach ($posts as $post) : ?>
-    <article>
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
 
-     <h1><a href="/posts/<?=$post->slug;?>"> <?= $post->title; ?></a></h1>
+            <h1><a href="/posts/<?= $post->slug ?>">
+                    {{ $post->title }}
+                </a></h1>
 
-        <div>
-            <?= $post->body; ?>
-        </div>
-    </article>
-    <?php endforeach; ?>
+            <div>
+                {{ $post->excerpt }}
+
+            </div>
+
+        </article>
+    @endforeach
 </body>
