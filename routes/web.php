13d8dcbdd52237fa93b750b5974c45112052db4e
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-26 09:31:36 +0300

    caching-expensive-operations

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,17 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         // abort(404);
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    // $post = cache()->remember("posts.{$slug}", 1200, function () use ($path) {
+    //     var_dump('file_get_contents');
+    //     return file_get_contents($path);
+    // });
+    $post = cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
 
-    return view('post', [
-        'post' => $post,
-    ]);
-});
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z_\-]+');
