56f5255e6178ff145e30ea0b12d9269a20893ff1
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-28 00:04:34 +0300

    group-store-validation-logic

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -22,22 +22,22 @@ class AdminPostController extends Controller
 
     public function store()
     {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'thumbnail' => 'required|image',
-            'slug' => ['required', Rule::unique('posts', 'slug')],
-            'excerpt' => 'required',
-            'body' => 'required',
-            'category_id' => ['required', Rule::exists('categories', 'id')],
-        ]);
+        // $attributes = $this->validatePost();
 
-        $attributes['user_id'] = auth()->id();
+        // $attributes['user_id'] = auth()->id();
 
-        $attributes['thumbnail'] = request()
-            ->file('thumbnail')
-            ->store('thumbnails', 'public');
+        // $attributes['thumbnail'] = request()
+        //     ->file('thumbnail')
+        //     ->store('thumbnails', 'public');
 
-        Post::create($attributes);
+        Post::create(
+            array_merge($this->validatePost(), [
+                'user_id' => request()->user()->id,
+                'thumbnail' => request()
+                    ->file('thumbnail')
+                    ->store('thumbnails'),
+            ]),
+        );
 
         return redirect('/')->with('success', 'Success Message');
     }
@@ -51,15 +51,9 @@ class AdminPostController extends Controller
     /** @test */
     public function update(Post $post)
     {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post->id)],
-            'excerpt' => 'required',
-            'body' => 'required',
-            'category_id' => ['required', Rule::exists('categories', 'id')],
-        ]);
+        $attributes = $this->validatePost($post);
 
-        if (isset($attributes['thumbnail'])) {
+        if ($attributes['thumbnail'] ?? false) {
             $attributes['thumbnail'] = request()
                 ->file('thumbnail')
                 ->store('thumbnails', 'public');
@@ -75,5 +69,19 @@ class AdminPostController extends Controller
         $post->delete();
 
         return back()->with('success', 'Post Deleted!');
+    }
+
+    protected function validatePost(?Post $post = null): array
+    {
+        $post ??= new Post();
+
+        return request()->validate([
+            'title' => 'required',
+            'thumbnail' => $post->exists ? ['image'] : ['required', 'image'],
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post)],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')],
+        ]);
     }
 }
