303de95880655c63880a0c391240041c7ddf9df2
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-27 20:20:51 +0300

    acces-only-admins

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,16 +9,24 @@ class PostsController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search','category','author']))->paginate(6)->withQueryString(),
+            'posts' => Post::latest()
+                ->filter(request(['search', 'category', 'author']))
+                ->paginate(6)
+                ->withQueryString(),
         ]);
-
     }
 
     public function show(Post $post)
+
     {
         return view('posts.show', [
             'post' => $post,
         ]);
     }
 
+    public function create()
+
+    {
+        return view('posts.create');
+    }
 }
