dca49b2eeda49a8fcea8858e1e54eabcb0f6645f
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-27 17:07:51 +0300

    newsletter-service

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('/', [PostsController::class, 'index'])->name('home');
 
@@ -16,8 +21,8 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('login', [SessionsController::class,'create'])->middleware('guest');
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 
-Route::post('sessions', [SessionsController::class,'store'])->middleware('guest');
+Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
 
-Route::post('logout', [SessionsController::class,'destroy'])->middleware('auth');
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
