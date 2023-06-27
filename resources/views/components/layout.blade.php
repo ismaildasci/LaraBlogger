111b4c0d46df9bf20060dc092629be305ebc8c73
ismail <109577378+ismaildasci@users.noreply.github.com>
2023-06-27 22:56:53 +0300

    extend-admin-layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -32,17 +32,27 @@
             </div>
 
             <div class="mt-8 md:mt-0 flex items-center">
-                @auth
-                    <a class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}</a>
 
-                    <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
+                @auth
+                <x-dropdown>
+                    <x-slot name="trigger">
+                    <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}</button>
+                    </x-slot>
+                    <x-dropdown-item href="/admin/dashboard">Dashboard</x-dropdown-item>
+                    <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+                    <x-dropdown-item href="/logout" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
+                </x-dropdown>
+
+                    <form id="logout-form" method="POST" action="/logout" class="hidden">
                         @csrf
 
                         <button type="submit">Log Out</button>
                     </form>
                 @else
+
                     <a href="/register" class="text-xs font-bold uppercase">Register</a>
                     <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+
                 @endauth
 
                 <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,42 +1,34 @@
 <x-layout>
-    <section class="py-8 max-w-sm mx-auto">
+    <x-setting heading="Publish New Post">
 
-        <h1 class="text-lg font-bold mb-4">
-            Publish New Post
-        </h1>
+        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            @csrf
 
-        <x-panel>
+            <x-form.input name="title" />
+            <x-form.input name="slug" />
+            <x-form.input name="thumbnail" type="file" />
+            <x-form.textarea name="excerpt" />
+            <x-form.textarea name="body" />
 
-            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
+            <x-form.field>
 
-                @csrf
+                <x-form.label name="category" />
 
+                <select name="category_id" id="category_id">
+                    @foreach (\App\Models\Category::all() as $category)
+                        <option value="{{ $category->id }} {{ old('category_id') == $category->id ? 'selected' : '' }}">
+                            {{ ucwords($category->name) }}
+                        </option>
+                    @endforeach
 
-                <x-form.input name="title" />
-                <x-form.input name="slug" />
-                <x-form.input name="thumbnail" type="file" />
-                <x-form.textarea name="excerpt" />
-                <x-form.textarea name="body" />
+                </select>
+                <x-form.error name="category" />
 
-                <x-form.field>
+            </x-form.field>
 
-                    <x-form.label name="category" />
+            <x-form.button>Publish</x-formbutton>
+        </form>
 
-                    <select name="category_id" id="category_id">
-                        @foreach (\App\Models\Category::all() as $category)
-                            <option value="{{ $category->id }} {{ old('category_id') == $category->id ? 'selected' : '' }}">
-                                {{ ucwords($category->name) }}
-                            </option>
-                        @endforeach
-
-                    </select>
-                    <x-form.error name="category" />
-
-                </x-form.field>
-
-                <x-form.button>Publish</x-form.button>
-            </form>
-        </x-panel>
-    </section>
+    </x-setting>
 
 </x-layout>
